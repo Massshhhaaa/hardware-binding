ce61a14676935a3bfaa6dbde9e4c50fb8e297f99
Ivan Loseev <loseev5@gmail.com>
2021-12-04 17:26:57 +0300

    add sobmodule relative path

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import os
+import sys 
+
+WORKDIR = os.path.dirname(__file__)
+sys.path.append( WORKDIR )
+
+from src.check_license import checkLicense
 
 def authenticate(func):
-    def wrapper(*args, **kwargs):
-        from src.check_license import checkLicense
-        
-        WORKDIR = os.path.dirname(__file__)
+    def wrapper(*args, **kwargs):     
         path_to_license = os.path.join(WORKDIR, 'store/license.txt')
 
         if checkLicense(path_to_license):
