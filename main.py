7b7d9909519d4d4631bd6cc5d4c96632feea42d1
Ivan Loseev <57405750+spooky-finn@users.noreply.github.com>
2021-12-04 15:14:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os
 
 def authenticate(func):
-    def wrapper():
+    def wrapper(*args, **kwargs):
         from src.check_license import checkLicense
         
         WORKDIR = os.path.dirname(__file__)
         path_to_license = os.path.join(WORKDIR, 'store/license.txt')
 
         if checkLicense(path_to_license):
-            func()
+            func(*args, **kwargs)
             
     return wrapper
